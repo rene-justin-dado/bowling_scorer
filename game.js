a0e6bc156b3f4427b4d807f3e1dd38944b63f091
rene-justin-dado <justindado@yahoo.com>
2016-07-21 07:01:03 +1200

    added bowling score sheet to help logic

--- game.js
+++ game.js
@@ -3,54 +3,45 @@
 // var frames = [
 //   [1, 2], [6, 4], [5, 4], [10, 0], [7, 2], [10, 0], [10, 0], [5, 2], [7, 0], [4, 4]
 // ]
+// 3, 9, 13, 18, 27, 37, 55, 65, 85, 92, 99, 106, 114
 
 var frames = [
   [1, 2], [6, 4], [5, 4], [10, 0], [7, 2], [10, 0], [10, 0], [5, 2], [7, 0], [4, 4]
 ]
 
 var total = 0
-frames.forEach(function(element) {
+var strike = 0
+var spare = 0
+
+frames.forEach(function(element, index) {
+  if (strike === 10) {
+    total += element[0] + element[1]
+    console.log ('strikes count the next frame')
+  }
   element.reduce(function(prev, current, index, frames) {
-    var strike = 0
-    var spare = 0
     //adds current ball to next ball
-    if (strike === 10) {
-      (element [0] + element [1]) * 2
-      console.log ('strikes count the next frame')
-    }
+  
     total += element[index]
     
+    //Checks for a strike
     if (element[0] === 10) {
       strike = 10
-      console.log('this is a strike')
+      //Should only show above an iteration with a strike
+      console.log('this is a strike', strike)
     }
+    //Checks for a spare
     if (element[0] !== 10 && element[0] + element[1] === 10){ 
       spare = 10
+      //Should only show above an iteration with a spare
       console.log('this is a spare')
     }
+    //This is for understanding the reduce parameters. Element is the current element in the array that forEach is looping through
     console.log (prev, current, index, 'Score: ' + total, 'Element is ' + element)
   }, element)
 })
 
 
-// if(a === 10) {
-//   strike + (a + b)
-//   strike.push(10)
-// }
-// if(a + b === 10 && a !== 10) {
-//   spare + a
-//   spare.push(10)
-// }
-// total.push(a + b)
-
-//create arrays to store score ie arr = total
-// LOOP through frames
-// store strikes spares and reg score in seperate variables
-// add numbers at current index of array
-// IF certain variable(strike, spare) is acheived, add to previous score in specific fashion
-// IF no strike or spare occurs, add as normal
-// IF returned total is === to score, end operations
-
+//Test2
 // Score 141:
 //
 // var frames = [
