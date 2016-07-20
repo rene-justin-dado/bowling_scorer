dac24ba9805959aff41b8cb7d8b9819531c8dca4
rene-justin-dado <justindado@yahoo.com>
2016-07-21 06:21:29 +1200

    strikes and spares being tracked

--- game.js
+++ game.js
@@ -1,21 +1,35 @@
+//Original
 //Score 119:
+// var frames = [
+//   [1, 2], [6, 4], [5, 4], [10, 0], [7, 2], [10, 0], [10, 0], [5, 2], [7, 0], [4, 4]
+// ]
 
 var frames = [
   [1, 2], [6, 4], [5, 4], [10, 0], [7, 2], [10, 0], [10, 0], [5, 2], [7, 0], [4, 4]
 ]
 
 var total = 0
-frames.reduce(function(prev, current, index, frames) {
-
-  // if(prev === 10) {
-  //   var strike = prev.reduce(function (ball1, ball2) {ball1 + ball2})
-  //   console.log(strike, ' is a strike')
-  // } else if (prev !== 10 && prev + current === 10) {
-  //   //var spare = prev.reduce(function ())
-  //   console.log()
-  // }
-
-    console.log(prev, current, index, frames.length)
+frames.forEach(function(element) {
+  element.reduce(function(prev, current, index, frames) {
+    var strike = 0
+    var spare = 0
+    //adds current ball to next ball
+    if (strike === 10) {
+      (element [0] + element [1]) * 2
+      console.log ('strikes count the next frame')
+    }
+    total += element[index]
+    
+    if (element[0] === 10) {
+      strike = 10
+      console.log('this is a strike')
+    }
+    if (element[0] !== 10 && element[0] + element[1] === 10){ 
+      spare = 10
+      console.log('this is a spare')
+    }
+    console.log (prev, current, index, 'Score: ' + total, 'Element is ' + element)
+  }, element)
 })
 
 
